88023679ac0a1b59da9c2e0352a9880ac3950d52
Rafael Arduini <rafaelarduini@hotmail.com>
2020-12-30 17:52:00 -0300

    Estado e Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';
 
 function App(){
+  const [projects, setProjects] = useState(['Desenvolvimento de App','Front-end web']);
+
+  function handleAddProject() {
+    setProjects([...projects,`Novo projeto ${Date.now()}`]);
+  }
+
   return (
   <>
-    <Header title="Homepage">
-      <ul>
-        <li>Homepage</li>  
-        <li>Projects</li>  
-      </ul>  
-    </Header>
-    <Header title="Projects">
-      <ul>
-        <li>Homepage</li>  
-        <li>Projects</li>  
-        <li>Login</li>  
-      </ul>  
-    </Header>
+    <Header title="Projects"/>
+
+    <ul>
+      {projects.map(project => <li key={project}>{project}</li>)}
+    </ul>
+
+    <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
   </>
   );
 }
