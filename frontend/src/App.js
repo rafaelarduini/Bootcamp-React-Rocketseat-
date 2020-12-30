4bbe26565d27b4237b90e386de78b3f03bcc04c8
Rafael Arduini <rafaelarduini@hotmail.com>
2020-12-30 17:31:27 -0300

    Propriedades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 function App(){
   return (
   <>
-    <Header/>
-    <Header/>
+    <Header title="Homepage">
+      <ul>
+        <li>Homepage</li>  
+        <li>Projects</li>  
+      </ul>  
+    </Header>
+    <Header title="Projects">
+      <ul>
+        <li>Homepage</li>  
+        <li>Projects</li>  
+        <li>Login</li>  
+      </ul>  
+    </Header>
   </>
   );
 }
