341f1f4556d2dbc90ade040c7440b4145400a36f
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-02 08:17:27 -0300

    Listando Projetos da Api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const {v4: uuid, validate: isUuid} = require('uuid');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const projects =[];
