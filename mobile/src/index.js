206a7430ed734cb163116c0f9e627fb7d1675903
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-03 09:39:34 -0300

    Listando projetos da API

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {SafeAreaView,FlatList ,Text, StyleSheet, StatusBar} from 'react-native';
+
+import api from './services/api';
 
 export default function App(){
-  return <View />;
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() =>{
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          style={styles.container}
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project}) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+
+  project: {
+    color: '#FFF',
+    fontSize: 30,
+  }
+});
