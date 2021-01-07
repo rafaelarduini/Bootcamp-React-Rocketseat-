ae8a98236f71f754402a5fc697948576d53584ab
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-07 08:04:00 -0300

    Tipando Objetos e Vetores

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,20 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    name: 'Rafael',
+    email: 'rafael.arduini@hotmail.com',
+    password:'123456',
+    techs: [
+      'Node.js', 
+      'ReactJS', 
+      'React Native',
+      {title: 'JavaScript', experience: 100}
+    ],
+  });
+
+  console.log(user.email);
+
   return response.json({ message: 'Hello Wold'});
 }
