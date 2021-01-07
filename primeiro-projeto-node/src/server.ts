93d44337acb72285aa68ed5e710f194f5f79561f
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-07 09:37:31 -0300

    EditorConfig

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -3,9 +3,9 @@ import express, { request } from 'express';
 const app = express();
 
 app.get('/', (request, response) =>{
-  return response.json({message: 'Hello World'})
-})
+  return response.json({message: 'Hello World'});
+});
 
 app.listen(3333, () => {
-  console.log('🚀️ Server started on port 3333!')
-})
+  console.log('🚀️ Server started on port 3333!');
+});
