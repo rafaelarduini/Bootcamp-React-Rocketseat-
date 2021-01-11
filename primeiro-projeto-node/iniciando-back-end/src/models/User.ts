09eea96c09fc35bbd3efd5e0284ac625747f9318
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-11 17:07:31 -0300

    Cadastro de usuários

--- primeiro-projeto-node/iniciando-back-end/src/models/User.ts
+++ primeiro-projeto-node/iniciando-back-end/src/models/User.ts
@@ -7,16 +7,19 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   create_at: Date;
@@ -25,4 +28,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
