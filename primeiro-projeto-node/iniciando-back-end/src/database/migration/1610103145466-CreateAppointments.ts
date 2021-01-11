3b1df9cc5760be975d0c4b43b41d7bf19fcb0e1c
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-11 17:43:35 -0300

    Relacionamento nos Models

--- primeiro-projeto-node/iniciando-back-end/src/database/migration/1610103145466-CreateAppointments.ts
+++ primeiro-projeto-node/iniciando-back-end/src/database/migration/1610103145466-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1610103145466
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- primeiro-projeto-node/iniciando-back-end/src/database/migration/1610394088303-CreateUsers.ts
+++ primeiro-projeto-node/iniciando-back-end/src/database/migration/1610394088303-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1610394088303 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
