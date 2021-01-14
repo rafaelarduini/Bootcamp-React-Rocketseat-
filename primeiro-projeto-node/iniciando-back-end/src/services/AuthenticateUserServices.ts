0a3e5db0978954cc0d4b7213f84470e31527c3ab
Rafael Arduini <rafaelarduini@hotmail.com>
2021-01-14 07:46:54 -0300

    Gerando token Jwt

--- primeiro-projeto-node/iniciando-back-end/src/services/AuthenticateUserServices.ts
+++ primeiro-projeto-node/iniciando-back-end/src/services/AuthenticateUserServices.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -27,10 +30,16 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, '13240aa2a048ec8c35a24130a924dfc1', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     delete user.password;
 
     return {
       user,
+      token,
     };
   }
 }
